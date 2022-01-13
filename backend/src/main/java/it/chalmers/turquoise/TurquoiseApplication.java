c5ae098737474897519dede4110e6e24959cb502
Eric Carlsson <cirecirecire3@gmail.com>
2022-01-13 22:13:05 +0100

    initial spring setup

---
+++ backend/src/main/java/it/chalmers/turquoise/TurquoiseApplication.java
@@ -0,0 +1,13 @@
+package it.chalmers.turquoise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurquoiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurquoiseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/it/chalmers/turquoise/TurquoiseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.chalmers.turquoise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurquoiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
